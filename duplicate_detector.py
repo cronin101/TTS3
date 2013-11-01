74c1f21c41eb9529f5c3cb2305477f8e6ce8e86a
Aaron Cronin <acronin101@gmail.com>
2013-11-01 18:15:33 +0000

    Tupleprinter

--- duplicate_detector.py
+++ duplicate_detector.py
@@ -8,14 +8,15 @@ import unicodedata
 import hashlib
 from itertools import repeat, izip
 from operator import add
+from os import linesep
 
 class DocumentSimHasher:
   def __init__(self, retrieved_document, num_chunks):
     self.num_chunks = num_chunks
-    self.fingerprints = ([1 if bit == '1' else -1 for bit in f_print] for f_print in retrieved_document.fingerprints)
+    self.fingerprints = ((1 if bit == '1' else -1 for bit in f_print) for f_print in retrieved_document.fingerprints)
 
   def fingerprint_sum(self):
-    result = ''.join(['1' if bit > 0 else '0' for bit in (sum (e) for e in zip(*self.fingerprints))])
+    result = ''.join(('1' if bit > 0 else '0' for bit in (sum (e) for e in zip(*self.fingerprints))))
     # Return fingerprint sum in parts
     chunk_size = 224 / self.num_chunks
     return [result[i:i+chunk_size] for i in range(0, len(result), chunk_size)]
@@ -32,13 +33,13 @@ class DocumentRetrieval:
 
     # Remove 'Punctuation', 'Mark', 'Separator' and 'Other' unicode categories, and 'Math symbols' (<==>
     self.tokens = [token for token in normalised if not(_category(token)[0] in ['P', 'M', 'Z', 'C'] or _category(token) in ['Sm'])]
-    self.fingerprints = [bin(int(hashlib.sha224(str(ord(token))).hexdigest(), 16))[2:].zfill(224) for token in self.tokens]
+    self.fingerprints = (bin(int(hashlib.sha224(str(ord(token))).hexdigest(), 16))[2:].zfill(224) for token in self.tokens)
     self.contents = u''.join(self.tokens)
     self.filename = document_path.split('/')[-1]
 
 class IndexRetrieval:
   def __init__(self, index_path):
-    with open(index_path) as index:
+    with open(index_path, 'r') as index:
       index = index.readlines()
     print "Index contains " + str(len(index)) + " documents."
     self.index = [DocumentRetrieval(path.strip()) for path in index]
@@ -79,12 +80,16 @@ class ExactDupeDetector:
       if len(bucket) > 1:
         self.dupes.update(izip(repeat(bucket[0]), bucket[1:]))
 
+class TuplePrinter:
+  def __init__(self, filename, tuple_set):
+    with open(filename, 'w') as output:
+      output.write(linesep.join(' '.join([a,b]) for (a, b) in tuple_set) + linesep)
+
 
 def main():
   index = IndexRetrieval('./files.index').index
   dupes = ExactDupeDetector(index).dupes | SimHashDupeDetector(index).dupes
-  for dupe in dupes:
-    print dupe
+  TuplePrinter('./dupes', dupes)
 
 
 if __name__ == '__main__':
